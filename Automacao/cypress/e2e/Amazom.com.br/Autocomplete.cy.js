72972be765e1baf4267768ed4217fc0f3a2e41d0
Juliano Vilar <juliano.jose.vilar@gmail.com>
2025-03-28 14:57:39 -0300

    Atu xpath plugin

--- Automacao/cypress/e2e/Amazom.com.br/Autocomplete.cy.js
+++ Automacao/cypress/e2e/Amazom.com.br/Autocomplete.cy.js
@@ -1,11 +1,20 @@
 // Ajuda o VSCode a entender o que é o arquivo e a fornecer recursos de autocompletar
 /// <reference types="cypress" />
+/// <reference types="cypress-xpath" />
+require('cypress-xpath');
+import 'cypress-xpath';
 
 // Função para capturar prints de tela
 function capturarPrint() {
-  const timestamp = new Date().toISOString().replace(/[:.]/g, '-'); // Gera um timestamp único
-  const nomeDoPrint = `Tela_${timestamp}`; // Nome do arquivo começa com "Tela_" seguido do timestamp
-  cy.screenshot(nomeDoPrint);
+  // Gera um timestamp único
+  const timestamp = new Date().toISOString().replace(/[:.]/g, '-'); 
+  // Nome do arquivo começa com "Tela_" seguido do timestamp
+  const nomeDoPrint = `Tela_${timestamp}`; 
+  // Captura o print de tela e salva com o nome gerado
+  // Captura apenas o conteúdo visível
+  cy.screenshot(nomeDoPrint, { capture: 'viewport' }); 
+  // Captura o print de tela e salva com o nome gerado todo o conteúdo da página
+  //cy.screenshot(nomeDoPrint);
 }
 
 describe('RT - Autocomplete', () => {
@@ -14,29 +23,62 @@ describe('RT - Autocomplete', () => {
     cy.visit('https://www.amazon.com.br/')
   })
 
+  //Depois de cada teste, captura um print da tela
+  afterEach(() => {
+    capturarPrint();
+  })
+  
   //CT-0002
   it.skip('Verificar a apresentação do campo de pesquisa', () => {
     // Valida que o campo de pesquisa está visível
     cy.get('#twotabsearchtextbox').should('be.visible')
-     // Captura o print da tela
-     capturarPrint();
   })
 
   //CT-0003
-  it('Deve preencher o campo de pesquisa e selecionar um item', () => {
+  it.skip('Sugestões de pesquisa com caracteres', () => {
     // Preenche o campo de pesquisa
-    cy.get('#twotabsearchtextbox').type('iphone 11')
+    cy.get('#twotabsearchtextbox').type('a')
+    // Aguarda as sugestões aparecerem e valida que existem sugestões
+    cy.get('.s-suggestion').should('be.visible')
+  })
+
+  //Casos de CT-0004
+  it.skip('Deve preencher o campo de pesquisa e selecionar um item', () => {
+    // Preenche o campo de pesquisa
+    cy.get('#twotabsearchtextbox').type('iphone')
 
     // Aguarda as sugestões aparecerem e valida que existem sugestões
     cy.get('.s-suggestion').should('be.visible')
 
+    // Faz a captura da tela
+    capturarPrint()
+
+    // Captura a quantidade de sugestões
+    cy.get('.s-suggestion').its('length').then((qtdSugestoes) => {
+      // Valida que a quantidade de sugestões é maior que zero
+      expect(qtdSugestoes).to.be.greaterThan(0)
+    })
+   
     // Seleciona a primeira sugestão
     cy.get('.s-suggestion').first().click()
 
-    // Valida que a página de resultados foi carregada
-    cy.url().should('include', 'k=iphone+11')
-    
-    // Captura o print da tela
-    capturarPrint();
+    // Valida que a página de resultados contém parte do nome do item pesquisado
+    cy.get('span.a-color-state').should('contain.text', 'iphone'); 
   })
+
+  // CT-0005
+  it('Deve clicar no seletor "Todos"', () => {
+    // Clica no seletor "Todos" (Categorias)
+    //cy.xpath('//input[@id="nav-search-label-id"]').click();
+    //div[2]//div[2]//select[@id="searchDropdownBox"]
+    cy.xpath('//select[@id="searchDropdownBox"]').select('Todos os departamentos');
+    // Removed invalid @FindBy decorator
+    //cy.get('#nav-search-label-id').click();
+    // Valida que o seletor foi aberto
+    //cy.get('#nav-search-label-id').should('have.attr', 'aria-expanded', 'true');
+  });
+
+
+  
+
 })
